f3ebd87fe04239a31f11f01ac52d2baea87f73aa
Arnout van Westen <arnoutvw@gmail.com>
2020-04-20 21:43:20 +0200

    update to multiple domains.

--- main.py
+++ main.py
@@ -13,6 +13,20 @@ from selenium.webdriver.support.wait import WebDriverWait
 import configparser
 import argparse
 
+def get_product_id(maindomain):
+    productid_domain = None
+
+    products: List[WebElement] = browser.find_elements_by_class_name("product-list-item")
+
+    for product in products:
+        for spans in product.find_elements_by_tag_name("span"):
+            if spans.text == maindomain:
+                beheren_link = product.find_element_by_link_text("beheren")
+                # /pakketten/{id}/producten/dmp/instellingen
+                link = beheren_link.get_attribute("href")
+                productid_domain = link.split("/")[4]
+    return productid_domain
+
 parser = argparse.ArgumentParser(description='Update mijndomein DNS records')
 parser.add_argument('--config', type=str, help='The path to the config file', default="config.ini")
 
@@ -22,77 +36,79 @@ config.read(args.config)
 
 usernameValue = config['mijndomein']['username']
 passwordValue = config['mijndomein']['password']
-domainValue = config['mijndomein']['domain']
-mainDomain = domainValue
-subdomainValue = ''
-domain_value_split = domainValue.split(".")
-if len(domain_value_split) > 1:
-    subdomainValue = domain_value_split[0]
-    mainDomain = domain_value_split[1] + "." + domain_value_split[2]
+domains = config['mijndomein']['domains']
 
 opts = Options()
-opts.headless = False
+opts.headless = True
 browser = Firefox(options=opts)
 
 ip = get('https://api.ipify.org').text
 print('My public IP address is:', ip)
 
-browser.get('https://auth.mijndomein.nl/login')
-username = browser.find_element_by_css_selector("body > div > div > div > div > div:nth-child(3) > form > div:nth-child(3) > input")
-password = browser.find_element_by_css_selector("body > div > div > div > div > div:nth-child(3) > form > div:nth-child(4) > input")
-username.send_keys(usernameValue)
-password.send_keys(passwordValue)
-submit = browser.find_element_by_css_selector("body > div > div > div > div > div:nth-child(3) > form > div:nth-child(5) > button")
-submit.click()
+try:
+    browser.get('https://auth.mijndomein.nl/login')
+    username = browser.find_element_by_css_selector("body > div > div > div > div > div:nth-child(3) > form > div:nth-child(3) > input")
+    password = browser.find_element_by_css_selector("body > div > div > div > div > div:nth-child(3) > form > div:nth-child(4) > input")
+    username.send_keys(usernameValue)
+    password.send_keys(passwordValue)
+    submit = browser.find_element_by_css_selector("body > div > div > div > div > div:nth-child(3) > form > div:nth-child(5) > button")
+    submit.click()
 
-WebDriverWait(browser, 30).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "#my-packages > div.panel > ul > li > a > span.align-middle.ng-binding")))
+    WebDriverWait(browser, 30).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "#my-packages > div.panel > ul > li > a > span.align-middle.ng-binding")))
 
-products: List[WebElement] = browser.find_elements_by_class_name("product-list-item")
+    for domain in domains.split(","):
+        print("Running domain: ", domain.strip())
+        mainDomain = domain.strip()
+        subdomainValue = ''
+        domain_value_split = domain.strip().split(".")
+        if len(domain_value_split) > 1:
+            subdomainValue = domain_value_split[0]
+            mainDomain = domain_value_split[1] + "." + domain_value_split[2]
 
-productid = None
-for product in products:
-    for spans in product.find_elements_by_tag_name("span"):
-        if spans.text == mainDomain:
-            beherenLink = product.find_element_by_link_text("beheren")
-            # /pakketten/{id}/producten/dmp/instellingen
-            link = beherenLink.get_attribute("href")
-            productid = link.split("/")[4]
+        productid = get_product_id(mainDomain)
 
-if productid is None:
-    sys.exit("Product is not found on page.")
+        if productid is None:
+            sys.exit("Product is not found on page.")
 
-browser.get('https://mijnaccount.mijndomein.nl/portaal/dns-instellingen?packageId=' + productid)
+        browser.get('https://mijnaccount.mijndomein.nl/portaal/dns-instellingen?packageId=' + productid)
 
-iframe = WebDriverWait(browser, 30).until(expected_conditions.presence_of_element_located((By.ID, "portal-frame")))
-browser.switch_to.frame(iframe)
+        iframe = WebDriverWait(browser, 30).until(expected_conditions.presence_of_element_located((By.ID, "portal-frame")))
+        browser.switch_to.frame(iframe)
 
-WebDriverWait(browser, 30).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "Form_Table_Cell_title")))
+        WebDriverWait(browser, 30).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "Form_Table_Cell_title")))
 
-rows: List[WebElement] = browser.find_elements_by_xpath("//tr[contains(@class, 'row_')]")
+        rows: List[WebElement] = browser.find_elements_by_xpath("//tr[contains(@class, 'row_')]")
 
-changed = False
-for row in rows:
-    columns: List[WebElement] = row.find_elements_by_tag_name("td")
-    typerecord = columns[1]
-    subdomainrecord = columns[2]
-    if (typerecord.text == "A") and \
-            (subdomainrecord.text == subdomainValue):
-        editRecord = columns[9]
-        editRecord.find_element_by_class_name("ICON3_edit").click()
+        changed = False
+        for row in rows:
+            columns: List[WebElement] = row.find_elements_by_tag_name("td")
+            typerecord = columns[1]
+            subdomainrecord = columns[2]
+            if (typerecord.text == "A") and \
+                    (subdomainrecord.text == subdomainValue):
+                editRecord = columns[9]
+                editRecord.find_element_by_class_name("ICON3_edit").click()
 
-        ipInputField = columns[4].find_element_by_tag_name("input")
-        ipCurrentValue = ipInputField.get_attribute("value")
-        if ipCurrentValue != ip:
-            ipInputField.clear()
-            ipInputField.send_keys(ip)
+                ipInputField = columns[4].find_element_by_tag_name("input")
+                ipCurrentValue = ipInputField.get_attribute("value")
+                if ipCurrentValue != ip:
+                    ipInputField.clear()
+                    ipInputField.send_keys(ip)
 
-            editRecord.find_element_by_class_name("ICON3_diskette").click()
-            changed = True
-            break
-# endfor
+                    editRecord.find_element_by_class_name("ICON3_diskette").click()
+                    changed = True
+                    break
+        # endfor
 
-if changed:
-    browser.find_element_by_tag_name("form").submit()
-    sleep(30)
+        if changed:
+            browser.find_element_by_tag_name("form").submit()
+            sleep(30)
 
-browser.close()
+        browser.switch_to.parent_frame()
+        browser.get("https://mijnaccount.mijndomein.nl/pakketten")
+        WebDriverWait(browser, 30).until(expected_conditions.presence_of_element_located(
+            (By.CSS_SELECTOR, "#my-packages > div.panel > ul > li > a > span.align-middle.ng-binding")))
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+finally:
+    browser.close()
