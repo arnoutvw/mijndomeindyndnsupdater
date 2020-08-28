f707bc4f8a50e7bc434b6f86ae66607e0271c8a7
Arnout van Westen <arnoutvw@gmail.com>
2020-08-28 21:31:08 +0200

    changed config file to yml and made everthing faster with less page reloads.

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ from time import sleep
 from typing import List
 
 from requests import get
-from selenium.webdriver import Firefox
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+from selenium.webdriver import Firefox, ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-import configparser
 import argparse
+import confuse
+import logging
 
 def get_product_id(maindomain):
     productid_domain = None
@@ -28,19 +30,18 @@ def get_product_id(maindomain):
     return productid_domain
 
 parser = argparse.ArgumentParser(description='Update mijndomein DNS records')
-parser.add_argument('--config', type=str, help='The path to the config file', default="config.ini")
+parser.add_argument('--config', type=str, help='The path to the config file', default="config.yml")
 
 args = parser.parse_args()
-config = configparser.ConfigParser()
-config.read(args.config)
+config = confuse.load_yaml(args.config)
 
 usernameValue = config['mijndomein']['username']
 passwordValue = config['mijndomein']['password']
-domains = config['mijndomein']['domains']
 
 opts = Options()
-opts.headless = True
+opts.headless = False
 browser = Firefox(options=opts)
+browser.maximize_window()
 
 ip = get('https://api.ipify.org').text
 print('My public IP address is:', ip)
@@ -56,49 +57,50 @@ try:
 
     WebDriverWait(browser, 30).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "#my-packages > div.panel > ul > li > a > span.align-middle.ng-binding")))
 
-    for domain in domains.split(","):
-        print("Running domain: ", domain.strip())
-        mainDomain = domain.strip()
-        subdomainValue = ''
-        domain_value_split = domain.strip().split(".")
-        if len(domain_value_split) > 1:
-            subdomainValue = domain_value_split[0]
-            mainDomain = domain_value_split[1] + "." + domain_value_split[2]
-
+    for domainConfig in config['domains']:
+        mainDomain = domainConfig['domeinnaam']
+        print("Running main domain: ", mainDomain.strip())
         productid = get_product_id(mainDomain)
 
         if productid is None:
             sys.exit("Product is not found on page.")
 
+        changed = False
         browser.get('https://mijnaccount.mijndomein.nl/portaal/dns-instellingen?packageId=' + productid)
 
-        iframe = WebDriverWait(browser, 30).until(expected_conditions.presence_of_element_located((By.ID, "portal-frame")))
+        iframe = WebDriverWait(browser, 30).until(
+            expected_conditions.presence_of_element_located((By.ID, "portal-frame")))
         browser.switch_to.frame(iframe)
 
-        WebDriverWait(browser, 30).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "Form_Table_Cell_title")))
+        WebDriverWait(browser, 30).until(
+            expected_conditions.presence_of_element_located((By.CLASS_NAME, "Form_Table_Cell_title")))
 
-        rows: List[WebElement] = browser.find_elements_by_xpath("//tr[contains(@class, 'row_')]")
+        for subdomain in domainConfig['subdomeins']:
+            print("Running for subdomain: " + subdomain)
+            subdomainDivElements: List[WebElement] = browser.find_elements_by_xpath("//div[.='" + subdomain + "']")
 
-        changed = False
-        for row in rows:
-            columns: List[WebElement] = row.find_elements_by_tag_name("td")
-            typerecord = columns[1]
-            subdomainrecord = columns[2]
-            if (typerecord.text == "A") and \
-                    (subdomainrecord.text == subdomainValue):
-                editRecord = columns[9]
-                editRecord.find_element_by_class_name("ICON3_edit").click()
+            for subdomainDivElement in subdomainDivElements:
+                trElement: WebElement = subdomainDivElement.find_element_by_xpath("../..")
+                columns: List[WebElement] = trElement.find_elements_by_tag_name("td")
+                typerecord = columns[1]
+                browser.execute_script("arguments[0].scrollIntoView(true);", typerecord)
+                sleep(0.5)
+                if typerecord.text == "A":
+                    editRecord = columns[9]
+                    editRecord.find_element_by_class_name("ICON3_edit").click()
 
-                ipInputField = columns[4].find_element_by_tag_name("input")
-                ipCurrentValue = ipInputField.get_attribute("value")
-                if ipCurrentValue != ip:
-                    ipInputField.clear()
-                    ipInputField.send_keys(ip)
+                    sleep(5)
+                    ipInputField = columns[4].find_element_by_tag_name("input")
+                    ipCurrentValue = ipInputField.get_attribute("value")
+                    if ipCurrentValue != ip:
+                        ipInputField.clear()
+                        ipInputField.send_keys(ip)
 
-                    editRecord.find_element_by_class_name("ICON3_diskette").click()
-                    changed = True
-                    break
-        # endfor
+                        editRecord.find_element_by_class_name("ICON3_diskette").click()
+                        changed = True
+                    else:
+                        columns[8].find_element_by_class_name("ICON3_undo").click()
+        #endfor
 
         if changed:
             browser.find_element_by_tag_name("form").submit()
@@ -108,7 +110,8 @@ try:
         browser.get("https://mijnaccount.mijndomein.nl/pakketten")
         WebDriverWait(browser, 30).until(expected_conditions.presence_of_element_located(
             (By.CSS_SELECTOR, "#my-packages > div.panel > ul > li > a > span.align-middle.ng-binding")))
-except:
-    print("Unexpected error:", sys.exc_info()[0])
+    #endfor
+except Exception:
+    logging.exception("Fatal error in main loop", exc_info=True)
 finally:
     browser.close()
